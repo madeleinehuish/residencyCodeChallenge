9b7e4ea467d7dae9948934a42a9e5367fcd7618c
madeleinehuish <vasistha@hotmail.com>
2017-01-12 20:37:02 -0800

    solved it oh ya

--- residencyCodeChallenge.js
+++ residencyCodeChallenge.js
@@ -2,63 +2,53 @@
 <output id="list"></output>
 
 <script>
+
   function handleFileSelect(evt) {
-    var files = evt.target.files; // FileList object
-
-    // files is a FileList of File objects. List some properties.
-    var output = [];
-    for (var i = 0, f; f = files[i]; i++) {
-      output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
-                  f.size, ' bytes, last modified: ',
-                  f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
-                  '</li>');
-    }
-    document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
-  }
-
-  document.getElementById('files').addEventListener('change', handleFileSelect, false);
-</script>
-
-<style>
-  .thumb {
-    height: 75px;
-    border: 1px solid #000;
-    margin: 10px 5px 0 0;
-  }
-</style>
-
-<input type="file" id="files" name="files[]" multiple />
-<output id="list"></output>
-
-<script>
-  function handleFileSelect(evt) {
-    var files = evt.target.files; // FileList object
-
-    // Loop through the FileList and render image files as thumbnails.
+    var files = evt.target.files;
     for (var i = 0, f; f = files[i]; i++) {
 
-      // Only process image files.
-      if (!f.type.match('image.*')) {
-        continue;
-      }
+      // if (!f.type.match('json.*')) {
+      //   continue;
+      // } else {
+      //   alert('Please use a JSON file.');
+      // }
 
       var reader = new FileReader();
 
       // Closure to capture the file information.
       reader.onload = (function(theFile) {
+
         return function(e) {
-          // Render thumbnail.
-          var span = document.createElement('span');
-          span.innerHTML = ['<img class="thumb" src="', e.target.result,
-                            '" title="', escape(theFile.name), '"/>'].join('');
-          document.getElementById('list').insertBefore(span, null);
+          // var span = document.createElement('span');
+
+          var obj = JSON.parse(e.target.result);
+
+          function nested(nestedObject){
+            if(typeof nestedObject.content === 'object') {
+              if(Array.isArray(nestedObject.content)) {
+                let arr = nestedObject.content;
+                for(let i=0; i < arr.length; i++) {
+                  nested(arr[i]);
+                }
+              } else {
+                nested(nestedObject.content);
+              }
+            } else {
+              let tag = nestedObject.tag;
+              let content = nestedObject.content;
+              let domElement = document.body.appendChild(document.createElement(tag));
+              domElement.innerHTML = content;
+            }
+          }
+
+          for(let i=0; i < obj.length; i++){
+            nested(obj[i]);
+          }
+
         };
       })(f);
-
-      // Read in the image file as a data URL.
-      reader.readAsDataURL(f);
+      reader.readAsBinaryString(f);
     }
   }
-
   document.getElementById('files').addEventListener('change', handleFileSelect, false);
 </script>
